acddc3aa67e32f4a73d29ab9b93dc510045013da
o-sk <m.ohsuka@gmail.com>
2018-12-18 22:36:44 +0900

    Display body

--- gis.go
+++ gis.go
@@ -1,6 +1,8 @@
 package gis
 
 import (
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 )
@@ -15,10 +17,16 @@ const GOOGLE_URL = "https://www.google.com/search?&tbm=isch&q="
 
 func Search(query string) ([]Image, error) {
 	url := GOOGLE_URL + url.QueryEscape(query)
-	_, err := http.Get(url)
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("%#v", string(body))
 	images := []Image{}
 	return images, nil
 }
