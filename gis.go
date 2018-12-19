6d02c02ce51903a7fd490ca5997b6606b04f37ff
o-sk <m.ohsuka@gmail.com>
2018-12-19 21:49:21 +0900

    Fake download

--- gis.go
+++ gis.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/url"
+	"runtime"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -14,6 +16,11 @@ type Image struct {
 	Destination string `json:"ru"`
 	Source      string `json:"ou"`
 	Thumbnail   string `json:"tu"`
+}
+
+type DownloadResult struct {
+	FilePath string
+	Error    error
 }
 
 const GOOGLE_URL = "https://www.google.com/search?&tbm=isch&q="
@@ -40,4 +47,86 @@ func Search(query string) ([]Image, error) {
 		images[i] = image
 	})
 	return images, nil
+}
+
+func Download(images []Image) []DownloadResult {
+	download := func(done <-chan interface{}, imageStream <-chan Image) <-chan DownloadResult {
+		downloadStream := make(chan DownloadResult)
+		go func() {
+			defer close(downloadStream)
+			for image := range imageStream {
+				// TODO: Download processing
+				downloadResult := DownloadResult{FilePath: image.Description, Error: nil}
+				select {
+				case <-done:
+					return
+				case downloadStream <- downloadResult:
+				}
+			}
+		}()
+		return downloadStream
+	}
+
+	fanIn := func(done <-chan interface{}, channels ...<-chan DownloadResult) <-chan DownloadResult {
+		var wg sync.WaitGroup
+		multiplexedStream := make(chan DownloadResult)
+		mulitiplex := func(c <-chan DownloadResult) {
+			defer wg.Done()
+			for i := range c {
+				select {
+				case <-done:
+					return
+				case multiplexedStream <- i:
+				}
+			}
+		}
+		wg.Add(len(channels))
+		for _, c := range channels {
+			go mulitiplex(c)
+		}
+		go func() {
+			wg.Wait()
+			close(multiplexedStream)
+		}()
+		return multiplexedStream
+	}
+
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan DownloadResult,
+		num int,
+	) <-chan DownloadResult {
+		takeStream := make(chan DownloadResult)
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	imageStream := make(chan Image, len(images))
+	defer close(imageStream)
+	for _, image := range images {
+		imageStream <- image
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	numChannel := runtime.NumCPU()
+	downloaders := make([]<-chan DownloadResult, numChannel)
+	for i := 0; i < numChannel; i++ {
+		downloaders[i] = download(done, imageStream)
+	}
+	downloadResults := make([]DownloadResult, len(images))
+	for downloadResult := range take(done, fanIn(done, downloaders...), len(images)) {
+		downloadResults = append(downloadResults, downloadResult)
+	}
+	return downloadResults
 }
